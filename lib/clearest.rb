e947dafc137a38d7495bc7b59fe4316c62be5d84
Admin Ultragreen <admin@ultragreen>
2014-05-20 12:47:15 +0200

    refactoring

--- lib/clearest.rb
+++ lib/clearest.rb
@@ -1,5 +1,5 @@
-require "./clearest/version"
-require './template'
+require "clearest/version"
+require 'template'
 require 'pp'
 require 'date'
 require 'data_mapper'
@@ -11,9 +11,9 @@ require 'dm-sqlite-adapter'
 
 
 
-Dir[File.dirname(__FILE__) + '/patches/*_patch.rb'].sort.each {|file| p file ;require file }
-Dir[File.dirname(__FILE__) + '/generators/*.rb'].sort.each {|file| p file ;require file }
-Dir[File.dirname(__FILE__) + '/builders/*.rb'].sort.each {|file| p file;require file }
+Dir[File.dirname(__FILE__) + '/clearest/patches/*_patch.rb'].sort.each {|file| require file }
+Dir[File.dirname(__FILE__) + '/clearest/generators/*.rb'].sort.each {|file| require file }
+Dir[File.dirname(__FILE__) + '/clearest/builders/*.rb'].sort.each {|file| require file }
 
 
 
